293bcc780300b481443dedb910b6425dbf5101ad
domenicole <memimika0809@gmail.com>
2026-07-16 11:56:52 -0500

    Actualizar proyecto

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
-const express = required('express');
+const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 app.get('/', (_req,res) =>{
-    res.send('Integración contínua funcional');
+    res.status(200).json({
+        mensaje: 'CI./CD funcionando correctamente',
+        servicio: 'API Express desplegado en Vercel'
+    });
 });
+
+app.get('/saludo/:nombre', (_req,res) =>{
+    const { nombre } = _req.params;
+    res.status(200).json({
+        mensaje: `¡Hola, ${nombre}`,
+        servicio: 'API disponible'
+    });
+});
+
+app.get('/suma', (_req,res) =>{
+    const numero1 = Number(_req.query.numero1);
+    const numero2 = Number(_req.query.numero2);
+    
+    if(Number.isNaN(numero1) || Number.isNaN(numero2)){
+        return res.status(400).json({
+            error: 'Los parámetros numero1 y numero2 deben ser números válidos'
+        });
+    }
+
+    res.status(200).json({
+        numero1,
+        numero2,
+        resultado: numero1 + numero2
+    });
+});
+
 
 app.listen(PORT, () =>{
     console.log('Servidor ejecutando en: http://localhost:${PORT}');
